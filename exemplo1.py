110f623d1c98e9439f340eae2a99b3802fbe3c2d
Guilherme Dutra <guilhermedutra812@gmail.com>
2021-01-27 14:52:50 -0300

    criando arquyivo inicial em python

---
+++ exemplo1.py
@@ -0,0 +1,4 @@
+nome = input("qual o seu nome? ")
+end = input("qual seu emdereço? ")
+idade = input("qual sua idade? ")
+print("Olá seu nome é ",nome, "voce mora no endereço: ",end, " e tem ", idade, "anos\n\n")
