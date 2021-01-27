d6aa5724801d7ceab395f7651db7c3328ee8a33a
Guilherme Dutra <guilhermedutra812@gmail.com>
2021-01-27 16:19:27 -0300

    exercicio 1 resolvido

---
+++ exercicio 1.py
@@ -0,0 +1,4 @@
+comprimento = float(input("informe o comprimento do terreno: "))
+largura = float(input("informe a largura :"))
+custo = (comprimento * largura)*850
+print("o custo total para construir a casa é: ", custo)
