548a4d3e60398685764ddb2e481efeeb198f4e60
Guilherme Dutra <guilhermedutra812@gmail.com>
2021-02-01 13:56:10 -0300

    exercicio 4 resolvido

---
+++ exercicio 4.py
@@ -0,0 +1,7 @@
+c = float(input("informe a temperatura em celsius: "))
+
+k = float(c + 273)
+print("a temperatura em farahneit é: ", k)
+
+f = float(1,80 * c + 32)
+print("a temperatura em kelvin é: ", f)
